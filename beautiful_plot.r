c87d29a83270648ba7f4d897fd8d805bb60de8bc
Leo Andrey <leoandreybio@gmail.com>
2025-11-10 15:49:26 +0100

    Boxplot of tree species

---
+++ beautiful_plot.r
@@ -0,0 +1,6 @@
+set.seed(1234)
+height <- c(rnorm(30, 15, 4), rnorm(30, 11, 3), rnorm(30, 10, 1))
+species <- as.factor(c(rep("Picea abies", 30), rep("Fagus sylvatica", 30), rep("Quercus robur", 30)))
+df <- data.frame(height, species)
+
+boxplot(height~species, data = df)
