68e07dba40e8f0be1a1c6f4143b69c81169b3278
Leo Andrey <leoandreybio@gmail.com>
2025-11-11 11:18:07 +0100

    Generating image for powerpoint

--- beautiful_plot.r
+++ beautiful_plot.r
@@ -1,3 +1,4 @@
+# bonjour
 set.seed(1234)
 height <- c(rnorm(30, 15, 4), rnorm(30, 11, 3), rnorm(30, 10, 1))
 species <- as.factor(c(rep("Picea abies", 30), rep("Fagus sylvatica", 30), rep("Quercus robur", 30)))
